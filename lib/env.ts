c703e61540c95f3ac17429f03dbc6d40e4bf63a8
Keith Vergara <kdv062997@gmail.com>
2026-05-01 12:23:48 +0800

    resend

--- lib/env.ts
+++ lib/env.ts
@@ -1,6 +1,6 @@
 export const env = {
   RESEND_API_KEY: process.env.RESEND_API_KEY ?? "",
-  CONTACT_TO_EMAIL: process.env.CONTACT_TO_EMAIL ?? "keithvergara1997@gmail.com",
+  CONTACT_TO_EMAIL: process.env.CONTACT_TO_EMAIL ?? "kdv062997@gmail.com",
   CONTACT_FROM_EMAIL: process.env.CONTACT_FROM_EMAIL ?? "onboarding@resend.dev",
 } as const;
 
